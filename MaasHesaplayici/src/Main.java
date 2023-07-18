58b23fd9567dd06f22f3a8a0d49ad393bbea1017
Sena <125875987+senaemirmusaogullari@users.noreply.github.com>
2023-07-18 14:27:41 +0300

    Maaş Hesaplayıcı

---
+++ MaasHesaplayici/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee("Ayşe", 9000, 30, 2013-01-01);
+		Employee e2 = new Employee("Ahmet", 20000, 50, 2007-01-01);
+		
+		e1.printInfo();
+		e2.printInfo();
+		
+
+	}
+
+}
